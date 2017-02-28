e9125996619e86e43867548dad7d11024919b94c
Bryan Feuling <bfeuling@gmail.com>
2017-02-28 14:46:57 -0600

    Updated TAB

--- DEP 2.0.0.sh
+++ DEP 2.0.0.sh
@@ -11,36 +11,36 @@
 LoggedInUser=$(/usr/libexec/PlistBuddy -c "print :dsAttrTypeStandard\:RealName:0" /dev/stdin <<< "$(dscl -plist . -read /Users/$(stat -f%Su /dev/console) RealName)")
 
 function SetProvision() {
-  sudo /usr/libexec/PlistBuddy -c "Set :ProvisioningScript 2.0.0" -c "Set :Status Provisioned" /usr/local/ti/com.ti.provisioned.plist
+	sudo /usr/libexec/PlistBuddy -c "Set :ProvisioningScript 2.0.0" -c "Set :Status Provisioned" /usr/local/ti/com.ti.provisioned.plist
 }
 
 function CompName() {
-  CompType=$(/usr/sbin/system_profiler SPHardwareDataType | grep "Model Name")
-  SerialNumber=$(/usr/sbin/system_profiler SPHardwareDataType | awk '/Serial/ {print $4}')
-  if [ "${CompType}" == *"MacBook"* ]; then
-    CompName="L${SerialNumber}"
-  else
-    CompName="D${SerialNumber}"
-  fi
+	CompType=$(/usr/sbin/system_profiler SPHardwareDataType | grep "Model Name")
+	SerialNumber=$(/usr/sbin/system_profiler SPHardwareDataType | awk '/Serial/ {print $4}')
+	if [ "${CompType}" == *"MacBook"* ]; then
+		CompName="L${SerialNumber}"
+ 	else
+		CompName="D${SerialNumber}"
+ 	fi
 	/usr/sbin/scutil --set ComputerName  "${CompName}"
 	/usr/sbin/scutil --set LocalHostName "${CompName}"
 	/usr/sbin/scutil --set HostName "${CompName}"
-  /usr/bin/defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName "${CompName}"
+	/usr/bin/defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName "${CompName}"
 }
 
 function LockScreen() {
-  sudo /System/Library/CoreServices/RemoteManagement/AppleVNCServer.bundle/Contents/Support/LockScreen.app/Contents/MacOS/LockScreen -session 256
+	sudo /System/Library/CoreServices/RemoteManagement/AppleVNCServer.bundle/Contents/Support/LockScreen.app/Contents/MacOS/LockScreen -session 256
 }
 
 JSSAPIpass="${4}"
 
 function APICall() {
-  jssURL="https://jssdmz.ext.ti.com:8443/JSSResource"
-  serial=$(/usr/sbin/system_profiler SPHardwareDataType | awk '/Serial/ {print $4}')
-  jssAPIUser="jssapi"
-  jssAPIPass="${JSSAPIpass}"
+	jssURL="https://jssdmz.ext.ti.com:8443/JSSResource"
+	serial=$(/usr/sbin/system_profiler SPHardwareDataType | awk '/Serial/ {print $4}')
+	jssAPIUser="jssapi"
+	jssAPIPass="${JSSAPIpass}"
 
-  curl -X PUT -H "Accept: application/xml" -H "Content-type: application/xml" -k -u "${jssAPIUser}:${jssAPIPass}" -d "<computer><extension_attributes><attribute><name>${1}</name><value>${2}</value></attribute></extension_attributes></computer>" "${jssURL}"/computers/serialnumber/"${serial}"
+	curl -X PUT -H "Accept: application/xml" -H "Content-type: application/xml" -k -u "${jssAPIUser}:${jssAPIPass}" -d "<computer><extension_attributes><attribute><name>${1}</name><value>${2}</value></attribute></extension_attributes></computer>" "${jssURL}"/computers/serialnumber/"${serial}"
 }
 
 function Recon() {
@@ -51,16 +51,16 @@ function ScreenSize() {
 	ModelQuery=$(system_profiler SPHardwareDataType | grep "Model Name")
 	ModelName="${ModelQuery##*:}"
 
-  resolution=$(system_profiler SPDisplaysDataType | grep Resolution | awk '{print$2,$3,$4}')
-  ScreenSize=`
-    case "${resolution}" in
-      "1366 x 768")                  echo "11";;
-      "2560 x 1600" | "1440 x 900")  echo "13";;
-      "2880 x 1800")                 echo "15";;
-      "1920 x 1080" | "4096 x 2304") echo "21";;
-      "5120 x 2880")                 echo "27";;
-    esac
-    `
+	resolution=$(system_profiler SPDisplaysDataType | grep Resolution | awk '{print$2,$3,$4}')
+	ScreenSize=`
+	case "${resolution}" in
+		"1366 x 768")                  echo "11";;
+		"2560 x 1600" | "1440 x 900")  echo "13";;
+		"2880 x 1800")                 echo "15";;
+    	"1920 x 1080" | "4096 x 2304") echo "21";;
+    	"5120 x 2880")                 echo "27";;
+	esac
+	`
 
 	Model="${ScreenSize}\"${ModelName}"
 
